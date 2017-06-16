12041e5d3dcd042a35491bd3ca29337fe93fd5f1
TurboJambon <david.chirol@wanadoo.fr>
2017-06-16 11:46:50 +0200

    koi

--- ex03/cookie_crisp.php
+++ ex03/cookie_crisp.php
@@ -1,9 +1,13 @@
 <?php
 if ($_GET["action"] == "set")
-	setcookie($_GET["name"], $_GET["value", time()]);
+{
+	setcookie($_GET["name"], $_GET["value"], time());
+}
 if ($_GET["action"] == "get")
+{
 	if ($_COOKIE[$_GET["name"]])
 		print($_COOKIE[$_GET["name"]]."\n");
+}
 if ($_GET["action"] == "del")
 	setcookie($_GET["name"], "", time() - 3600);
 ?>
